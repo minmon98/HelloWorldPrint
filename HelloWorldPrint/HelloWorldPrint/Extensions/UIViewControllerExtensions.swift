c2cb0d8b916125e7493245221d0af0268126b379
Minh Mon <minhmon@192.168.2.101>
2021-05-30 12:34:10 +0700

    first commit

---
+++ HelloWorldPrint/HelloWorldPrint/Extensions/UIViewControllerExtensions.swift
@@ -0,0 +1,15 @@
+//
+//  UIViewControllerExtensions.swift
+//  HelloWorldPrint
+//
+//  Created by Minh Mon on 30/05/2021.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    func helloWorldPrint() {
+        print("Hello World")
+    }
+}
